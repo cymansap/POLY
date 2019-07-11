b2b92fb76aee94d84d36550adc91ebbd4a73307e
cyman <cyman@cosmiccrew.net>
2019-07-11 07:42:46 -0700

    jumping

--- main.lua
+++ main.lua
@@ -15,15 +15,31 @@ function love.load()
         {0,1,0, 1},
         {1,0,1, 1}
     }
+    GRAVITY = WINDOW_H * 10
+    JUMP_HEIGHT = WINDOW_H * 0.5
+    JUMP_VEL = math.sqrt(2*GRAVITY*JUMP_HEIGHT)
 
     poly = {}
 
     start()
 end
 
+function love.update(dt)
+    if not poly.grounded then
+        poly.yv = poly.yv + GRAVITY*dt/2
+        poly.y = poly.y + poly.yv*dt
+        poly.yv = poly.yv + GRAVITY*dt/2
+        if poly.y > GROUND_H then
+            poly.y = GROUND_H
+            poly.grounded = true
+        end
+    end
+end
+
 function love.draw()
     love.graphics.setColor(1, 1, 1)
     love.graphics.line(0,GROUND_H, WINDOW_W,GROUND_H)
+
     love.graphics.setColor(COLORS[poly.state])
     love.graphics.push()
     love.graphics.translate(poly.x, poly.y)
@@ -31,19 +47,29 @@ function love.draw()
     love.graphics.pop()
 end
 
+function love.keypressed(key)
+    if key=='space' then jump()
+    elseif key=='s' then setState(1)
+    elseif key=='d' then setState(2)
+    elseif key=='f' then setState(3)
+    end
+end
+
 function start()
     poly.state = 1
     poly.x = WINDOW_W * 0.17
     poly.y = GROUND_H
+    poly.yv = 0
+    poly.grounded = true
 end
 
 function setState(state)
     poly.state = state
 end
 
-function love.keypressed(key)
-    if key=='s' then setState(1)
-    elseif key=='d' then setState(2)
-    elseif key=='f' then setState(3)
+function jump()
+    if poly.grounded then
+        poly.yv = -JUMP_VEL
+        poly.grounded = false
     end
 end
